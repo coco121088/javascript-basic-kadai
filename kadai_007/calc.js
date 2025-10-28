8f5465ed2dd81f9494e522116b065884dedf33ac
coco121088 <coco1210@ymail.ne.jp>
2025-10-29 03:14:12 +0900

    課題ファイル07再々提出を追加

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
-let num=Math.floor(Math.random()*100);
+let num=Math.floor(Math.random()*100)+1;
 
 console.log(num);
 
   //変数numが3と5の倍数の場合
-if(num%15==0&& num%3==0){console.log('3と5の倍数です');}
+if(num % 15 == 0){console.log('3と5の倍数です');}
 
 //3の倍数の場合
-else if(num%3==0){console.log('3の倍数です');}
+else if(num % 3 == 0){console.log('3の倍数です');}
 
 //5の倍数の場合
-else if(num%5==0){console.log('5の倍数です');}
+else if(num % 5 == 0){console.log('5の倍数です');}
 
 //その他の場合
 else{console.log(num);}
